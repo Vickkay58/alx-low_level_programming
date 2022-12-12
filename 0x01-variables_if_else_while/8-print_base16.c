738c37aecfacc5a8e81560f01ebd50a9265181d0
vickkay58@gmail.com <vickkay58@gmail.com>
2022-12-12 15:41:12 -0800

    variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point 
+ * 
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num = '0'; /*Initialize num to 0*/
+	char letter = 'a'; /*Initialize letter to a*/
+
+	while (num <= '9') /*Print 0-9*/
+	{
+		putchar(num);
+		num++;
+	}
+	while (letter <= 'f') /*Print a-f*/
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
