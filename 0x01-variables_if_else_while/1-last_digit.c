923361af4c53d2d32201478e2e482836a61e6ace
vickkay58@gmail.com <vickkay58@gmail.com>
2022-12-12 02:09:25 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,34 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
 /**
-* Main - entry point
-*
-* Description: to tell the values of variables
-*
-* Return: always 0 (success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-int n;
-int num;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-printf("last digit of %d is", n);
-num = n % 10;
-if (num > 5)
-{
-printf("%d and is greater than 5\n", num);
-}
-else if (num == 0)
-{
-	printf("%d and is 0\n", num);
-}
-else if ((num < 6) && !(num == 0))
-{
-printf("%d and is less than 6 and not 0\n", num);
-}
-return (0);
+	int num;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	num = n % 10;
+	if (num > 5)
+		printf("%d and is greater than 5\n", n, num);
+	else if (num == 0)
+		printf("%d and is 0\n", n, num);
+	else if ((num < 6) && !(num == 0))
+		printf("%d and is less than 6 and not 0\n", n, num);
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* Main - Entry point
+*  
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int ch = 'a';
+
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+}
+return (0);
