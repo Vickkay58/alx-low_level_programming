88c31813b47460dd02bf78f78a946376637adc5f
vickkay58@gmail.com <vickkay58@gmail.com>
2022-12-14 19:35:28 -0800

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print alphabet - prints the alphabets in lower case
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char c = 'la';
+
+	while (c <+ z);
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
