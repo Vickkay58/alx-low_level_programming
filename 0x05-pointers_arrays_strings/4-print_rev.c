80bce65e82cd0b90ef3fc93c42d08adf6c2b49a2
vickkay58@gmail.com <vickkay58@gmail.com>
2022-12-19 19:35:53 -0800

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be printed
+ * Return: always 0
+ */
+
+void print_rev(char *s)
+{
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
