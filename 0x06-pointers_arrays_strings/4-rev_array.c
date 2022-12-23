b665b261c06c324aea851789aa76a74592533d57
vickkay58@gmail.com <vickkay58@gmail.com>
2022-12-22 16:17:35 -0800

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * reverse_array - reverse an array
  * @a: Pointer to array
@@ -7,7 +6,6 @@
  *
  * Return: void
  */
-
 void reverse_array(int *a, int n)
 {
 	int temp, counter;
